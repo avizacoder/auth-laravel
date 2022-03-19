4663b43e7de01e1c190d6282500ca4afb54c8f4b
leonel <leonnelgma>
2022-03-19 17:41:35 -0400

    create user role
    fix #1

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,11 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'number_phone' => $this->faker->e164PhoneNumber,
+            'cedula_identidad' => $this->faker->ean8,
+            'date_birt' => $this->faker->date(),
+            'city_code' => $this->faker->randomDigit,
+            'role' => $this->faker->randomElement(['client']),
         ];
     }
 
